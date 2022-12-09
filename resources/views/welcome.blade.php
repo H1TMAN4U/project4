9ecbabb7fc4b995c6e99dd1e87cbae71a05ea7f3
H1TMAN4U <andris.e.artejevs@gmail.com>
2022-12-09 11:46:08 +0200

    replace project3 with project4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                 </div>
             @endif
 
-            {{-- {{-- @extends('layouts/navigation1') --}}
+            <x-navigation-layout></x-navigation-layout>
             @section('content')
             <section class="intro" style="height: 95vh;">
                 <div class="" style="padding: 5% 5% 0 5%" >
@@ -101,7 +101,7 @@
                 </div>
             </div>
             </section>
-            
+
             <footer class="p-4 shadow md:px-6 md:py-8 dark:bg-white-100">
                 <span class="block text-sm text-gray-700 sm:text-center dark:text-gray-700">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
                 </span>

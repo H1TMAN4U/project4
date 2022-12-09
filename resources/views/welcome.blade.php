3d65147ae522a5ed2048b4b7b6b9a5cf1e328670
H1TMAN4U <Andris.Artejevs@valmierastehnikums.lv>
2022-12-09 18:25:21 +0200

    footer fixed, guest navigation kind of fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,11 +95,5 @@
             </div>
         </section>
 
-        <footer class="p-4 shadow md:px-6 md:py-8 dark:bg-gray-100">
-            <span class="block text-sm text-gray-700 sm:text-center dark:text-gray-700">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-            </span>
-        </footer>
-    </body>
-</html>
-
 </x-guest-layout>
+@extends('layouts/footer')
